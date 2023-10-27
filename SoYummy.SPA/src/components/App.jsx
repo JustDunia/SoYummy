73c3330d9f38fe54af1a196d9876205e04863fd2
anapisarek <anabeblo@gmail.com>
2023-10-27 12:42:12 +0200

    welcomePage

--- SoYummy.SPA/src/components/App.jsx
+++ SoYummy.SPA/src/components/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router-dom'
 import { SharedLayout } from '../components/SharedLayout'
 import { lazy } from 'react'
+import { RestrictedRoute } from '../service/routes'
+import { WelcomePage } from '../pages/WelcomePage'
 
 export const App = () => {
 	// dynamiczny import komponentów
@@ -13,6 +15,7 @@ export const App = () => {
 				{/* Do podanych ścieżek należy dodać elementy, które będą na nich renderowane */}
 				<Route path='/' element={<SharedLayout />}>
 					<Route index element={<Home />} />
+					<Route path="/welcome" element={<RestrictedRoute component={<WelcomePage />} />}/>
 					<Route path='/register' element={<></>} />
 					<Route path='/signin' element={<></>} />
 					<Route path='/categories/:categoryName' element={<></>} />

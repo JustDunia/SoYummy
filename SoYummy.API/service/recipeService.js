b4fbc657a679d770de9fdbbe5f5bd914346e5a3c
Dominik Szajbler <110773381+dominikszajbler@users.noreply.github.com>
2023-10-31 23:58:50 +0100

    Update recipeService.js

--- SoYummy.API/service/recipeService.js
+++ SoYummy.API/service/recipeService.js
@@ -1,15 +1,12 @@
 const Recipe = require('../models/recipe');
 
-async function getRecipeById(recipeId) {
-    return Recipe.findById(recipeId);
-  }
+const getRecipeById = async (recipeId) => Recipe.findById(recipeId);
 
-  async function getRecipeCategories() {
-    console.log('Rozpoczęcie pobierania kategorii');
-    const categories = await Recipe.distinct('category').sort();
-    return categories;
-    
-  }
+const getRecipeCategories = async () => {
+  console.log('Rozpoczęcie pobierania kategorii');
+  const categories = await Recipe.distinct('category').sort();
+  return categories;
+};
 
 module.exports = {
   getRecipeById,

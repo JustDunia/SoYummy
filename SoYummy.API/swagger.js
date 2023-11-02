8dc2ab4602a353c7c453e1e63d5da7d288639215
Dominik Szajbler <110773381+dominikszajbler@users.noreply.github.com>
2023-11-02 19:30:46 +0100

    add swagger

--- SoYummy.API/swagger.js
+++ SoYummy.API/swagger.js
@@ -7,7 +7,38 @@ const swaggerOptions = {
 			title: 'SoYummy Project',
 			version: '1.0.0',
 		},
+		components: {
+			schemas: {
+			  Recipe: {
+				type: 'object',
+				properties: {
+				  title: {
+					type: 'string',
+				  },
+				  category: {
+					type: 'string',
+				  },
+				  area: {
+					type: 'string',
+				  },
+				  instructions: {
+					type: 'string',
+				  },
+				  thumb: {
+					type: 'string',
+				  },
+				  // inne pola przepisu
+				},
+				// inne opcje definicji
+			  },
+			},
+		  },
 	},
-	apis: ['./routes/auth.js'],
+	apis: [
+		'./routes/auth.js',
+		'./routes/recipes.js',
+		'./routes/search.js',
+	],
+	
 }
 module.exports = swaggerJsdoc(swaggerOptions)

64fb697ef6f283a6aa1c92a28d6c4a51b22b6a24
Dominik Szajbler <110773381+dominikszajbler@users.noreply.github.com>
2023-11-04 09:17:33 +0100

    added support for any path in swagger

--- SoYummy.API/swagger.js
+++ SoYummy.API/swagger.js
@@ -1,44 +1,39 @@
-const swaggerJsdoc = require('swagger-jsdoc')
+const swaggerJsdoc = require("swagger-jsdoc");
 
 const swaggerOptions = {
-	definition: {
-		openapi: '3.0.0',
-		info: {
-			title: 'SoYummy Project',
-			version: '1.0.0',
-		},
-		components: {
-			schemas: {
-			  Recipe: {
-				type: 'object',
-				properties: {
-				  title: {
-					type: 'string',
-				  },
-				  category: {
-					type: 'string',
-				  },
-				  area: {
-					type: 'string',
-				  },
-				  instructions: {
-					type: 'string',
-				  },
-				  thumb: {
-					type: 'string',
-				  },
-				  // inne pola przepisu
-				},
-				// inne opcje definicji
-			  },
-			},
-		  },
-	},
-	apis: [
-		'./routes/auth.js',
-		'./routes/recipes.js',
-		'./routes/search.js',
-	],
-	
-}
-module.exports = swaggerJsdoc(swaggerOptions)
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "SoYummy Project",
+      version: "1.0.0",
+    },
+    components: {
+      schemas: {
+        Recipe: {
+          type: "object",
+          properties: {
+            title: {
+              type: "string",
+            },
+            category: {
+              type: "string",
+            },
+            area: {
+              type: "string",
+            },
+            instructions: {
+              type: "string",
+            },
+            thumb: {
+              type: "string",
+            },
+            // inne pola przepisu
+          },
+          // inne opcje definicji
+        },
+      },
+    },
+  },
+  apis: ["./routes/*.js"],
+};
+module.exports = swaggerJsdoc(swaggerOptions);

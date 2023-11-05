9fbb3d4c144f18d80316cafbd7429dc28e5a6859
Dominik Szajbler <110773381+dominikszajbler@users.noreply.github.com>
2023-11-05 13:11:02 +0100

    kolejność wywoływania

--- SoYummy.API/controllers/shoppingListController.js
+++ SoYummy.API/controllers/shoppingListController.js
@@ -6,15 +6,16 @@ const authenticate = require('../middleware/authenticate');
  * Dodawanie składnika do listy zakupów
  */
 const addToShoppingList = async (req, res, next) =>  {
-  const { ingredientId, measure } = req.body;
   const userId = req.user.id;
+  const { ingredientId, measure } = req.body;
 
   console.log('Request to add ingredient to shopping list:', userId, ingredientId, measure);
 
   try {
     const result = await shoppingListService.addToShoppingList(userId, ingredientId, measure);
+    console.log(userId, ingredientId, measure);
     console.log('Ingredient added to shopping list successfully:', result);
-    res.status(200).json(result);
+    return res.status(201).json(result);
   } catch (e) {
     console.error(e);
     next(e);
@@ -25,8 +26,9 @@ const addToShoppingList = async (req, res, next) =>  {
  * Usuwanie składnika z listy zakupów
  */
 const removeFromShoppingList = async (req, res, next) =>  {
+  const userId = req.user.id;
   const { ingredientId } = req.body;
-  const userId = req.user.id; 
+   
 
   console.log('Request to remove ingredient from shopping list:', userId, ingredientId);
 

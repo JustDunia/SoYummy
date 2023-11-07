d3b507d545613dbd3878dbfc5b69c685deca5fcd
Dominik Szajbler <110773381+dominikszajbler@users.noreply.github.com>
2023-11-07 20:27:07 +0100

    ukrycie nawigacji na WelcomePage

--- SoYummy.SPA/src/components/SharedLayout.jsx
+++ SoYummy.SPA/src/components/SharedLayout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import { Suspense } from 'react'
 import Navigation from '../components/Navigation/Navigation'
 
@@ -7,11 +7,16 @@ import { SubscriptionButton } from './SubscriptionButton/SubscriptionButton.jsx'
 import { UserMenuMock } from './UserMenuMock'
 
 export const SharedLayout = () => {
+	const location = useLocation();
+	const isWelcomePage = location.pathname ==='/';
+	console.log('location.pathname:', location.pathname);
 	return (
 		<div>
-			<UserMenuMock />
-			<SubscriptionButton />
-			<Navigation />
+			{/* <UserMenuMock />
+			<SubscriptionButton /> */}
+			{/* Warunkowe renderowanie nawigacji na stronach innych niż strona startowa */}
+			{isWelcomePage ? null : <Navigation />}
+			
 			<Suspense fallback={<div>Loading...</div>}>
 				<Outlet />
 			</Suspense>

ae4d0a9b6d31032855e7d0695cf5656ae7932e16
Justyna <justyna.sienkiewicz93@gmail.com>
2023-11-07 02:10:15 +0100

    ingredients list mobile

--- SoYummy.SPA/src/pages/RecipePage/Components/IngredientsList/IngredientsList.jsx
+++ SoYummy.SPA/src/pages/RecipePage/Components/IngredientsList/IngredientsList.jsx
@@ -1,17 +1,25 @@
 import css from './IngredientsList.module.css'
 import Ingredient from '../Ingredient/Ingredient'
 
+import { useSelector } from 'react-redux'
+import { selectRecipe } from '../../../../redux/recipes/recipes.selectors'
+
 const IngredientsList = () => {
+	const recipe = useSelector(selectRecipe)
+	const ingredients = recipe.ingredients
 	return (
 		<div className={css.ingredientsList}>
 			<div className={css.listHeader}>
 				<p>Ingredients</p>
-				<div className={css.listHeaderRight}> 
-          <p>Number</p>
-          <p>Add to list</p>
-        </div>
+				<div className={css.listHeaderRight}>
+					<p>Number</p>
+					<p>Add to list</p>
+				</div>
 			</div>
-			<Ingredient />
+			{ingredients &&
+				ingredients.map(ingredient => (
+					<Ingredient key={ingredient.id._id} ingredient={ingredient} />
+				))}
 		</div>
 	)
 }

87d10870e3a02fc2c55fd2a83b7fb7b44d16a09e
Justyna <justyna.sienkiewicz93@gmail.com>
2023-11-08 17:48:01 +0100

    fix shared layout

--- SoYummy.SPA/src/components/SharedLayout.jsx
+++ SoYummy.SPA/src/components/SharedLayout.jsx
@@ -7,23 +7,16 @@ import { SubscriptionButton } from './SubscriptionButton/SubscriptionButton.jsx'
 import { UserMenuMock } from './UserMenuMock'
 
 export const SharedLayout = () => {
-	const location = useLocation();
-	const isWelcomePage = location.pathname ==='/' || location.pathname ==='/register' || location.pathname ==='/signin';
-	console.log('location.pathname:', location.pathname);
+	const location = useLocation()
+	const isWelcomePage =
+		location.pathname === '/' ||
+		location.pathname === '/register' ||
+		location.pathname === '/signin'
+	console.log('location.pathname:', location.pathname)
 	return (
 		<div>
-			{/* <UserMenuMock />  */}
-			{/* <SubscriptionButton /> */}
 			<Navigation />
-			
-
-			{isWelcomePage ? null :<UserMenuMock />}
-			{isWelcomePage ? null :<SubscriptionButton />}
-			{isWelcomePage ? null :<Navigation />}
-			
-
-			
-					<Suspense fallback={<div>Loading...</div>}>
+			<Suspense fallback={<div>Loading...</div>}>
 				<Outlet />
 			</Suspense>
 		</div>

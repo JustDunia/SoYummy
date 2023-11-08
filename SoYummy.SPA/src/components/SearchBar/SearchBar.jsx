d4cdf279da1e789f2657df73803fb8fcb58af339
Justyna <justyna.sienkiewicz93@gmail.com>
2023-11-08 18:43:20 +0100

    fixed search

--- SoYummy.SPA/src/components/SearchBar/SearchBar.jsx
+++ SoYummy.SPA/src/components/SearchBar/SearchBar.jsx
@@ -1,54 +1,47 @@
-import css from "../SearchBar/SearchBar.module.css";
-import PropTypes from "prop-types";
-import { useDispatch, useSelector } from "react-redux";
-import { selectSearchRecipes } from "../../redux/search/search.selectors";
-import { searchRecipes } from "../../redux/search/search.operations";
+import css from '../SearchBar/SearchBar.module.css'
+import PropTypes from 'prop-types'
+import { useDispatch, useSelector } from 'react-redux'
+import { selectSearchRecipes } from '../../redux/search/search.selectors'
+import { searchRecipes } from '../../redux/search/search.operations'
+import { useState } from 'react'
 
 const SearchBar = ({ btnColor }) => {
-	const dispatch = useDispatch();
-	// console.log(dispatch(searchRecipes("Chicken")));
-
-	const handleSearch = () => {
+	const dispatch = useDispatch()
+	const [keyword, setKeyword] = useState('')
+	const handleSearch = e => {
+		e.preventDefault()
+		console.log(e.target)
 		try {
-			dispatch(searchRecipes("Dessert"))
+			dispatch(searchRecipes(keyword))
 		} catch (error) {
-			console.log("errror", error);
+			console.log('errror', error)
 		}
 	}
+	const recipes = useSelector(selectSearchRecipes)
 
 	return (
-		<div
-			className={
-				btnColor === "black"
-					? css.searchBarContainer
-					: css.searchBarContainerGreen
-			}
-		>
+		<div className={btnColor === 'black' ? css.searchBarContainer : css.searchBarContainerGreen}>
 			<form className={css.bg}>
 				<input
 					className={css.searchBarInput}
-					type="text"
-					placeholder="Find recipe"
+					type='text'
+					placeholder='Find recipe'
+					onChange={e => setKeyword(e.target.value)}
 				/>
+				<button
+					className={btnColor === 'black' ? css.searchBarBtn : css.searchBarBtnGreen}
+					type='submit'
+					onClick={e => handleSearch(e)}
+				>
+					Search
+				</button>
 			</form>
-
-			<button
-				className={
-					btnColor === "black"
-						? css.searchBarBtn
-						: css.searchBarBtnGreen
-				}
-				type="submit"
-				onClick={() => handleSearch()}
-			>
-				Search
-			</button>
 		</div>
-	);
-};
+	)
+}
 
 SearchBar.propTypes = {
 	btnColor: PropTypes.string,
-};
+}
 
-export default SearchBar;
+export default SearchBar
--- SoYummy.SPA/src/redux/search/search.operations.js
+++ SoYummy.SPA/src/redux/search/search.operations.js
@@ -1,17 +1,14 @@
-import axios from "axios";
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios'
+import { createAsyncThunk } from '@reduxjs/toolkit'
 
-axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL = 'http://localhost:3000'
 
-export const searchRecipes = createAsyncThunk(
-  "recipes/search",
-  async ({ keyword }, thunkAPI) => {
-    try {
-      const res = await axios.get(`/search?keyword=${keyword}`);
-      console.log("RECIPES BY CATEGORY", res.data);
-      return res.data;
-    } catch (e) {
-      return thunkAPI.rejectWithValue(e.message);
-    }
-  }
-);
+export const searchRecipes = createAsyncThunk('search', async (keyword, thunkAPI) => {
+	try {
+		const res = await axios.get(`/search?keyword=${keyword}`)
+		console.log('RECIPES BY CATEGORY', res.data)
+		return res.data
+	} catch (e) {
+		return thunkAPI.rejectWithValue(e.message)
+	}
+})

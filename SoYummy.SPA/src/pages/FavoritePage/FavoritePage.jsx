a42a8060cebf556fca5dddfacf4d6d423a014a46
Justyna <justyna.sienkiewicz93@gmail.com>
2023-11-08 18:08:25 +0100

    fixed favorite

--- SoYummy.SPA/src/pages/FavoritePage/FavoritePage.jsx
+++ SoYummy.SPA/src/pages/FavoritePage/FavoritePage.jsx
@@ -1,46 +1,35 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { selectUserId } from "../../redux/auth/auth.selectors";
-import { selectFavoriteRecipes } from "../../redux/favorite/favorite.selectors";
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { selectFavoriteRecipes } from '../../redux/favorite/favorite.selectors'
 
-import { RecipesContainer } from "../../components/RecipesContainer/RecipesContainer";
-import {
-  getFavorite,
-  removeFromFavorites,
-} from "../../redux/favorite/favorite.operations";
-import { selectRecipesPopular } from "../../redux/recipes/recipes.selectors";
-import { getRecipesPopular } from "../../redux/recipes/recipes.operations";
+import { RecipesContainer } from '../../components/RecipesContainer/RecipesContainer'
+import { getFavorite, removeFromFavorites } from '../../redux/favorite/favorite.operations'
 
-import css from "./FavoritePage.module.css";
+import css from './FavoritePage.module.css'
 
 const FavoritePage = () => {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch()
 
-  useEffect(() => {
-    dispatch(getRecipesPopular());
-  }, [dispatch]);
+	useEffect(() => {
+		dispatch(getFavorite())
+	}, [dispatch])
 
-  // DO PODMIANKI NA FAVORITE
-  // useEffect(() => {
-  //   dispatch(getFavorite());
-  // }, [dispatch]);
+	const handleRemoveFav = e => {
+		e.preventDefault
+		dispatch(removeFromFavorites(recipeId))
+	}
 
-  const handleRemoveFav = (e) => {
-    e.preventDefault;
-    dispatch(removeFromFavorites(recipeId));
-  };
+	const recipes = useSelector(selectFavoriteRecipes)
 
-  const favoriteRecipes = useSelector(selectRecipesPopular).slice(0, 8);
+	return (
+		<div className={css.pageWrapper}>
+			<div>
+				<h2 className={css.pageTitle}>Favorite Recipes</h2>
+				<RecipesContainer recipes={recipes} remove={handleRemoveFav} />
+				{/* PAGINACJA */}
+			</div>
+		</div>
+	)
+}
 
-  return (
-    <div className={css.pageWrapper}>
-      <div>
-        <h2 className={css.pageTitle}>Favorite Recipes</h2>
-        <RecipesContainer recipes={favoriteRecipes} remove={handleRemoveFav} />
-        {/* PAGINACJA */}
-      </div>
-    </div>
-  );
-};
-
-export default FavoritePage;
+export default FavoritePage
--- SoYummy.SPA/src/redux/favorite/favorite.operations.js
+++ SoYummy.SPA/src/redux/favorite/favorite.operations.js
@@ -1,90 +1,34 @@
-import axios from "axios";
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import { selectUserId } from "../auth/auth.selectors";
+import axios from 'axios'
+import { createAsyncThunk } from '@reduxjs/toolkit'
 
-axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL = 'http://localhost:3000'
 
-const setAuthHeader = (token) => {
-  // console.log("Setting auth header with token:", token);
-  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-};
-
-const clearAuthHeader = () => {
-  // console.log("TRYING CLER HEADERS");
-  axios.defaults.headers.common.Authorization = "";
-};
-
-export const addToFavorites = createAsyncThunk(
-  "favorite/add",
-  async (recipeId, thunkAPI) => {
-    const state = thunkAPI.getState();
-    const userId = selectUserId(state);
-
-    try {
-      const res = await axios.put(`/favorite/${recipeId}`);
-      // console.log("ADD FAVORITE:", res.data);
-      return res.data;
-    } catch (e) {
-      return thunkAPI.rejectWithValue(e.message);
-    }
-  }
-);
+export const addToFavorites = createAsyncThunk('favorite/add', async (recipeId, thunkAPI) => {
+	try {
+		const res = await axios.put(`/favorite/${recipeId}`)
+		return res.data
+	} catch (e) {
+		return thunkAPI.rejectWithValue(e.message)
+	}
+})
 
 export const removeFromFavorites = createAsyncThunk(
-  "favorite/remove",
-  async (recipeId, thunkAPI) => {
-    const state = thunkAPI.getState();
-    const userId = selectUserId(state);
-    try {
-      const res = await axios.patch(`/favorite/${recipeId}`);
-      // console.log("REMOVED", res.data);
-      return res.data;
-    } catch (e) {
-      return thunkAPI.rejectWithValue(e.message);
-    }
-  }
-);
+	'favorite/remove',
+	async (recipeId, thunkAPI) => {
+		try {
+			const res = await axios.patch(`/favorite/${recipeId}`)
+			return res.data
+		} catch (e) {
+			return thunkAPI.rejectWithValue(e.message)
+		}
+	}
+)
 
-export const getFavorite = createAsyncThunk(
-  "favorite/get",
-  async (_, thunkAPI) => {
-    // const state = thunkAPI.getState();
-    // const token = state.auth.token;
-    // const userId = selectUserId(state);
-    // console.log("USER ID FROM FAV", userId);
-
-    try {
-      console.log("GET FAVORITE:", res.data.user.token);
-      const res = await axios.get("/favorite");
-      setAuthHeader(res.data.user.token);
-      // console.log("GET FAVORITE:", res.data.favorites);
-
-      return res.data;
-    } catch (e) {
-      return thunkAPI.rejectWithValue(e.message);
-    }
-  }
-);
-
-// export const getFavorite = createAsyncThunk(
-//   "favorite/get",
-//   async (_, thunkAPI) => {
-//     const state = thunkAPI.getState();
-//     const userId = selectUserId(state);
-//     console.log("USER ID", userId);
-
-//     if (!userId) {
-//       return thunkAPI.rejectWithValue("User ID is not available.");
-//     }
-
-//     try {
-//       // Przykład, jeśli `userId` jest wymagane w URL
-//       const res = await axios.get(`/favorite/${userId}`);
-//       console.log("GET FAVORITE:", res.data.favorites);
-//       return res.data.favorites; // upewnij się, że zwracasz właściwą część odpowiedzi
-//     } catch (e) {
-//       console.error("Error fetching favorites:", e.message);
-//       return thunkAPI.rejectWithValue(e.message);
-//     }
-//   }
-// );
+export const getFavorite = createAsyncThunk('favorite/get', async (_, thunkAPI) => {
+	try {
+		const res = await axios.get('/favorite')
+		return res.data
+	} catch (e) {
+		return thunkAPI.rejectWithValue(e.message)
+	}
+})

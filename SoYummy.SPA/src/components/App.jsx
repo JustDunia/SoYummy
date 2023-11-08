5adc4fd73a53a36a986cc43514d92a93a3acad2c
Justyna <justyna.sienkiewicz93@gmail.com>
2023-11-08 19:11:37 +0100

    .

--- SoYummy.SPA/src/components/App.jsx
+++ SoYummy.SPA/src/components/App.jsx
@@ -6,13 +6,13 @@ import css from './App.module.css'
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { currentUser } from '../redux/auth/auth.operations'
+import WelcomePage from '../pages/WelcomePage/WelcomePage'
+import SigninPage from '../pages/SigninPage/SigninPage'
+import RegisterPage from '../pages/RegisterPage/RegisterPage'
 
 export const App = () => {
-	const WelcomePage = lazy(() => import('../pages/WelcomePage/WelcomePage'))
 	const AddRecipiePage = lazy(() => import('../pages/AddRecipiePage/AddRecipiePage'))
 	const FavoritePage = lazy(() => import('../pages/FavoritePage/FavoritePage'))
-	const RegisterPage = lazy(() => import('../pages/RegisterPage/RegisterPage'))
-	const SigninPage = lazy(() => import('../pages/SigninPage/SigninPage'))
 	const MainPage = lazy(() => import('../pages/MainPage/MainPage'))
 	const CategoriesPage = lazy(() => import('../pages/CategoriesPage/CategoriesPage'))
 	const RecipePage = lazy(() => import('../pages/RecipePage/RecipePage'))
@@ -29,11 +29,11 @@ export const App = () => {
 	return (
 		<div>
 			<Routes>
+				<Route path='/welcome' element={<WelcomePage />} />
+				<Route path='/signin' element={<SigninPage />} />
+				<Route path='/register' element={<RegisterPage />} />
 				<Route path='/' element={<SharedLayout />}>
-					<Route index element={<WelcomePage />} />
-					<Route path='/register' element={<RegisterPage />} />
-					<Route path='/signin' element={<SigninPage />} />
-					<Route path='/main' element={<MainPage />} />
+					<Route index element={<MainPage />} />
 					<Route path='/categories/:categoryName' element={<CategoriesPage />} />
 					<Route path='/add' element={<AddRecipiePage />} />
 					<Route path='/favorite' element={<FavoritePage />} />
--- SoYummy.SPA/src/components/LogInForm/LogInForm.jsx
+++ SoYummy.SPA/src/components/LogInForm/LogInForm.jsx
@@ -1,71 +1,58 @@
-import { useDispatch, useSelector } from "react-redux";
-import { logIn } from "../../redux/auth/auth.operations";
-import { selectIsUserLoged } from "../../redux/auth/auth.selectors";
-import css from "./LogInForm.module.css";
-import { Link } from "react-router-dom";
-import icons from "../../images/commonSvgImg/icons.svg";
+import { useDispatch, useSelector } from 'react-redux'
+import { logIn } from '../../redux/auth/auth.operations'
+import { selectIsUserLoged } from '../../redux/auth/auth.selectors'
+import css from './LogInForm.module.css'
+import { Link } from 'react-router-dom'
+import icons from '../../images/commonSvgImg/icons.svg'
+import { useNavigate } from 'react-router-dom'
 
 const LogInForm = () => {
+	const dispatch = useDispatch()
+	const logged = useSelector(selectIsUserLoged)
+	const navigate = useNavigate()
 
-  const dispatch = useDispatch();
-  const logged = useSelector(selectIsUserLoged);
+	const handleSubmit = e => {
+		e.preventDefault()
+		const form = e.currentTarget
+		dispatch(
+			logIn({
+				email: form.elements.email.value,
+				password: form.elements.password.value,
+			})
+		)
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const form = e.currentTarget;
-    dispatch(
-      logIn({
-        email: form.elements.email.value,
-        password: form.elements.password.value,
-      })
-    );
+		console.log('IS USER LOGGED IN?:', logged)
+		form.reset()
+		navigate('/')
+	}
 
-    console.log("IS USER LOGGED IN?:", logged);
-    form.reset();
-  };
+	return (
+		<div>
+			<form className={css.signinForm} onSubmit={handleSubmit} autoComplete='off'>
+				<h2 className={css.signinTitle}>Sign in</h2>
+				<div className={css.inputContainer}>
+					<label className={css.label}>
+						<svg className={css.icon}>
+							<use href={`${icons}#icon-input-envelope`} />
+						</svg>
+						<input className={css.input} type='email' name='email' placeholder='Email' />
+					</label>
+					<label className={css.label}>
+						<svg className={css.icon}>
+							<use href={`${icons}#icon-input-lock`} />
+						</svg>
+						<input className={css.input} type='password' name='password' placeholder='Password' />
+					</label>
+				</div>
+				<button className={css.Button} type='submit'>
+					Log In
+				</button>
+			</form>
+			<Link to='/register' className={css.link}>
+				Registration
+			</Link>
+		</div>
+	)
+}
 
-  return (
-
-    <div>
-      <form
-        className={css.signinForm}
-        onSubmit={handleSubmit}
-        autoComplete="off"
-      >
-        <h2 className={css.signinTitle}>Sign in</h2>
-        <div className={css.inputContainer}>
-          <label className={css.label}>
-            <svg className={css.icon}>
-              <use href={`${icons}#icon-input-envelope`} />
-            </svg>
-            <input
-              className={css.input}
-              type="email"
-              name="email"
-              placeholder="Email"
-            />
-          </label>
-          <label className={css.label}>
-            <svg className={css.icon}>
-              <use href={`${icons}#icon-input-lock`} />
-            </svg>
-            <input
-              className={css.input}
-              type="password"
-              name="password"
-              placeholder="Password"
-            />
-          </label>
-        </div>
-        <button className={css.Button} type="submit">
-          Log In
-        </button>
-      </form>
-      <Link to="/register" className={css.link}>
-        Registration
-      </Link>
-    </div>
-  );
-};
-
-export default LogInForm;
+export default LogInForm

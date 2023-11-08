735a76f5579cb9a8c4ff8c00b0fe3c4c89f8b97b
Justyna <justyna.sienkiewicz93@gmail.com>
2023-11-08 20:00:24 +0100

    user

--- SoYummy.SPA/src/components/UserLogo/UserLogo.jsx
+++ SoYummy.SPA/src/components/UserLogo/UserLogo.jsx
@@ -1,27 +1,27 @@
-import css from "../UserLogo/UserLogo.module.css";
-import { useState } from "react";
-import { useSelector } from "react-redux";
-import UserLogoModal from "../UserLogoModal/UserLogoModal";
-import { selectUser } from "../../redux/auth/auth.selectors";
+import css from '../UserLogo/UserLogo.module.css'
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
+import UserLogoModal from '../UserLogoModal/UserLogoModal'
+import { selectUser } from '../../redux/auth/auth.selectors'
 
 const UserLogo = () => {
-	const [modalUserLogoOpen, setModalUserLogoOpen] = useState(false);
-	const user = useSelector(selectUser);
+	const [modalUserLogoOpen, setModalUserLogoOpen] = useState(false)
+	const user = useSelector(selectUser)
 
 	return (
 		<div className={css.userContainer}>
 			<div className={css.userLogoContainer}>
 				<img
 					className={css.userLogo}
-					src={"../src/images/default/default-small.jpg"}
-					alt="user logo"
+					src={'../src/images/boy.webp'}
+					alt='user logo'
 					onClick={() => setModalUserLogoOpen(!modalUserLogoOpen)}
 				/>
 			</div>
 			{modalUserLogoOpen && <UserLogoModal />}
-			<span className={css.username}> {user.username} </span>
+			<span className={css.username}> {user.username}</span>
 		</div>
-	);
-};
+	)
+}
 
-export default UserLogo;
+export default UserLogo

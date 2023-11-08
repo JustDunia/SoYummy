66cff1fb33422a3aa9a8498201635dfcd0468ade
Justyna <justyna.sienkiewicz93@gmail.com>
2023-11-08 18:11:12 +0100

    app.js

--- SoYummy.SPA/src/components/SharedLayout.jsx
+++ SoYummy.SPA/src/components/SharedLayout.jsx
@@ -1,20 +1,14 @@
-import { Outlet } from "react-router-dom";
-import { Suspense } from "react";
-import Navigation from "../components/Navigation/Navigation";
-
-import { SubscriptionButton } from "./SubscriptionButton/SubscriptionButton.jsx";
-
-import { UserMenuMock } from "./UserMenuMock";
+import { Outlet } from 'react-router-dom'
+import { Suspense } from 'react'
+import Navigation from '../components/Navigation/Navigation'
 
 export const SharedLayout = () => {
-  return (
-    <div>
-      <UserMenuMock />
-      <SubscriptionButton />
-      <Navigation />
-      <Suspense fallback={<div>Loading...</div>}>
-        <Outlet />
-      </Suspense>
-    </div>
-  );
-};
+	return (
+		<div>
+			<Navigation />
+			<Suspense fallback={<div>Loading...</div>}>
+				<Outlet />
+			</Suspense>
+		</div>
+	)
+}
